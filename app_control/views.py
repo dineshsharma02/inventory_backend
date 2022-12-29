11ae35c0e6145cfa860497b265d57dce8fab067e
dineshsharma02 <dineshsharma18987374@gmail.com>
2022-12-29 21:33:22 +0530

    Fix: inventory view page error

--- app_control/views.py
+++ app_control/views.py
@@ -19,9 +19,9 @@ class InventoryView(ModelViewSet):
         if self.request.method.lower()!="get":
             return self.queryset
         data = self.request.query_params.dict()
-        data.pop("page")
+        data.pop("page",None)
         keyword = data.pop("keyword",None)
-        results = self.queryset(**data)
+        results = self.queryset.filter(**data)
         if keyword:
             search_fields = (
                 "code","created_by__fullname","created_by__email","group__name","name"
